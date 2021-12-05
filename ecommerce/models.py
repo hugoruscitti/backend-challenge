a2d983c147de052f1a60fc28994eec79652a9a16
Hugo Ruscitti <hugoruscitti@gmail.com>
2021-12-04 23:23:21 -0300

    Create models and get_total method

--- ecommerce/models.py
+++ ecommerce/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=30)
+    price = models.FloatField(default=0)
+    stock = models.IntegerField(default=0)
+
+
+class Order(models.Model):
+    date_time = models.DateTimeField(auto_now_add=True)
+
+    def get_total(self):
+        return sum([detail.get_total() for detail in self.orderdetail_set.all()])
+
+
+class OrderDetail(models.Model):
+    order = models.ForeignKey('Order', on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=0)
+    product = models.ForeignKey('Product', on_delete=models.CASCADE)
+
+    def get_total(self):
+        return self.quantity * self.product.price
--- ecommerce/tests.py
+++ ecommerce/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from ecommerce import models
 
-# Create your tests here.
+class BasicTestCase(TestCase):
+
+    def test_get_total_in_orders(self):
+        watch = models.Product.objects.create(name="watch", price=99.00)
+        chair = models.Product.objects.create(name="chair", price=1000.00)
+
+        order = models.Order.objects.create()
+        order.orderdetail_set.create(product=watch, quantity=5)
+        order.orderdetail_set.create(product=chair, quantity=1)
+
+        self.assertEqual(1495.00, order.get_total())
