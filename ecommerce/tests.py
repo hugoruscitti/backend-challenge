bb2bc7cc29b6332a866e71c4b9ba84d187f7ae64
Hugo Ruscitti <hugoruscitti@gmail.com>
2021-12-05 18:16:51 -0300

    Add products endpoint

--- ecommerce/tests.py
+++ ecommerce/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from ecommerce import models
+from rest_framework.test import APITestCase
 
 class BasicTestCase(TestCase):
 
@@ -11,4 +12,24 @@ class BasicTestCase(TestCase):
         order.orderdetail_set.create(product=watch, quantity=5)
         order.orderdetail_set.create(product=chair, quantity=1)
 
-        self.assertEqual(1495.00, order.get_total())
+        self.assertEqual(order.get_total(), 1495.00)
+
+
+class ProductAPITestCase(APITestCase):
+
+    def test_list_no_products(self):
+        response = self.client.get("/api/products/")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data, [])
+
+    def test_list_with_one_product(self):
+        watch = models.Product.objects.create(name="watch", price=99.00)
+
+        response = self.client.get("/api/products/")
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), [{
+            "name": "watch",
+            "price": 99.0,
+            "stock": 0,
+        }])
--- ecommerce/views.py
+++ ecommerce/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework import serializers, viewsets
+from ecommerce import models
 
-# Create your views here.
+class ProductSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = models.Product
+        fields = [
+            'name', 
+            'price',
+            'stock',
+        ]
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = models.Product.objects.all()
+    serializer_class = ProductSerializer
